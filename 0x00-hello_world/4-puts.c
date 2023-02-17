8b3e73cb99e90b243613633a2b993bc96f220a79
jennylenzy <jenniferonyeonagu@gmail.com>
2023-02-16 16:32:18 -0800

    first line of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <studio.h>
-/*
- * main - print a line of code using puts
- *
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
  * Return: 0 
 */
 int main(void)
 {
-	puts("\|"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
